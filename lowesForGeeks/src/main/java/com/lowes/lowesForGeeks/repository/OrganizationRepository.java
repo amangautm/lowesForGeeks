4f921cf18ba1428b280bab80b60cfd92ea051a2e
Aman Gautam <37938543+amangautm@users.noreply.github.com>
2020-05-09 23:39:41 +0530

    Update OrganizationRepository.java

--- lowesForGeeks/src/main/java/com/lowes/lowesForGeeks/repository/OrganizationRepository.java
+++ lowesForGeeks/src/main/java/com/lowes/lowesForGeeks/repository/OrganizationRepository.java
@@ -1,5 +1,8 @@
 package com.lowes.lowesForGeeks.repository;
 
-public interface OrganizationRepository {
+import com.lowes.lowesForGeeks.model.Organization;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrganizationRepository extends CrudRepository<Organization,Integer> {
 
 }
